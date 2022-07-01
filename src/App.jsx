d89f526e2bd502298d99936811499604bd9329c0
Caroline Maia Abreu da Silva <carolmas99@gmail.com>
2022-07-01 16:32:14 -0300

    sidebar component  created

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,27 @@ import { useState } from 'react'
 import { Header } from './components/Header'
 import { Post } from './Post'
 
-import './global.css'
+import styles from './App.module.css';
+
+import './global.css';
+import { Sidebar } from './components/Sidebar';
 
 export function App() {
   return (
     <div>
       <Header/>
-      <Post author="Caroline Maia" content="Reference site about Lorem Ipsum, giving information on its origins, as well as a random Lipsum generator."/>
-      <Post author="Diego" content="Post muito legal"/>
+      
+      <div className={styles.wrapper}>
+        <Sidebar/>
+        <main>
+        <Post 
+          author="Caroline Maia"
+          content="Reference site about Lorem Ipsum, giving information on its origins, as well as a random Lipsum generator."/>
+        <Post 
+          author="Diego"
+          content="Post muito legal"/>
+        </main>
+      </div>
     </div>
   )
 }
