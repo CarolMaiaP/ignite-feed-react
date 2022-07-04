c40f785018d2096417b895620e091f1917ade80c
Caroline Maia Abreu da Silva <carolmas99@gmail.com>
2022-07-04 17:17:48 -0300

    comments form created

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -30,6 +30,16 @@ export function Post(){
           <a href="#">#rocketseat</a>{' '}
         </p>
       </div>
+
+      <form className={styles.comentForm}>
+        <strong>Deixe seu feedback</strong>
+        <textarea
+          placeholder='Deixe um comentário'
+        />
+        <footer>
+          <button type='submit'>Publicar</button>
+        </footer>
+      </form>
     </article>
   )
 }
