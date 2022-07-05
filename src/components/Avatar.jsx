af0bce7c1fb92167b87998f1990309e401485f3e
Caroline Maia Abreu da Silva <carolmas99@gmail.com>
2022-07-05 18:16:42 -0300

    destructuring assignment

--- src/components/Avatar.jsx
+++ src/components/Avatar.jsx
@@ -1,8 +1,7 @@
 import styles from './Avatar.module.css';
 
-export function Avatar(props){
-  const hasBorder = props.hasBorder !== false;
+export function Avatar({ hasBorder = true, src }){
   return(
-    <img className={hasBorder ? styles.avatarWithBorder : styles.avatar } src={props.src}/>
+    <img className={hasBorder ? styles.avatarWithBorder : styles.avatar } src={src}/>
   )
 }
