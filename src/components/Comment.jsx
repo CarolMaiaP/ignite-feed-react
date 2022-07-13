5c04937b5cd4a3cbbf657322309f4882ed0e6fe7
Caroline Maia Abreu da Silva <carolmas99@gmail.com>
2022-07-13 15:38:16 -0300

    Like function created

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,12 +1,18 @@
 import { parseWithOptions } from 'date-fns/fp';
 import { ThumbsUp, Trash } from 'phosphor-react';
+import { useState } from 'react';
 import { Avatar } from './Avatar';
 import styles from './Comment.module.css';
 
 export function Comment({content, onDeleteComment}){
+  const [likeCount, setLikeCount] = useState(0);
 
   function handleDeleteComment(){
     onDeleteComment(content)
+  }
+
+  function handleLikeComment(){
+    console.log(setLikeCount(likeCount + 1));
   }
 
   return(
@@ -31,9 +37,9 @@ export function Comment({content, onDeleteComment}){
         </div>
 
         <footer>
-          <button>
+          <button onClick={handleLikeComment}>
             <ThumbsUp/>
-            Aplaudir <span>20</span>
+            Aplaudir <span>{likeCount}</span>
           </button>
         </footer>
       </div>
